c86e69222c52aa32fec064a6dde22830eac2de5a
n1ved <nivedrsalini@gmail.com>
2023-06-17 22:21:31 +0530

    Update Images'

--- lib/screens/loading.dart
+++ lib/screens/loading.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:skyze/screens/home.dart';
 import 'package:skyze/util/location_worker.dart';
