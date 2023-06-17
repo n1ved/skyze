9a9376e4125cb7f0df0c8be5587a195933395b90
n1ved <nivedrsalini@gmail.com>
2023-06-17 20:44:47 +0530

    renderflex overflow fixed

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -91,6 +91,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: Container(
         decoration: BoxDecoration(
             image: DecorationImage(
