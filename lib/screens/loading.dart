a0f26f130523aa94a9a6e58653933012cdc58d4c
n1ved <nivedrsalini@gmail.com>
2023-06-17 21:15:36 +0530

    Loading page design done

--- lib/screens/loading.dart
+++ lib/screens/loading.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:skyze/screens/home.dart';
 import 'package:skyze/util/location_worker.dart';
 import 'package:skyze/util/data_worker.dart';
@@ -12,6 +13,8 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  String loadingText = 'Loading';
+
   @override
   void initState() {
     super.initState();
@@ -19,8 +22,14 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getData() async {
+    setState(() {
+      loadingText = 'Finding location';
+    });
     LocationLoader locationLoader = LocationLoader();
     var locationData = await locationLoader.getLocation();
+    setState(() {
+      loadingText = 'Requesting location data';
+    });
     GetData getData = GetData();
     dynamic returnData = await getData.getAllData(locationData);
     moveToHome(returnData);
@@ -34,27 +43,43 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(
-            FontAwesomeIcons.sun,
-            color: Colors.white,
-            size: 100,
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          Text(
-            "Loading",
-            style: TextStyle(
-              color: Colors.white,
-              decoration: TextDecoration.none,
-              fontSize: 20,
+    return Container(
+      color: Theme.of(context).colorScheme.primary,
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Skyze',
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.onPrimary,
+                decoration: TextDecoration.none,
+                fontSize: 72,
+              ),
             ),
-          )
-        ],
+            const SizedBox(height: 30),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                LoadingAnimationWidget.halfTriangleDot(
+                  size: 20,
+                  color: Theme.of(context).colorScheme.onPrimary,
+                ),
+                const SizedBox(width: 10),
+                Text(
+                  loadingText,
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.onPrimary,
+                    decoration: TextDecoration.none,
+                    fontWeight: FontWeight.w100,
+                    fontSize: 15,
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
