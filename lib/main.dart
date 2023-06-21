26f528a102c5bdc31604d8dd395751c398e5c1c8
n1ved <nivedrsalini@gmail.com>
2023-06-21 20:14:48 +0530

    dynamic color removed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
 import 'package:skyze/screens/loading.dart';
 
@@ -11,20 +10,11 @@ class SkyzeApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DynamicColorBuilder(
-      builder: (lightDynamic, darkDynamic) {
-        return MaterialApp(
-          theme: ThemeData(
-            colorScheme: lightDynamic,
-            useMaterial3: true,
-          ),
-          darkTheme: ThemeData(
-            colorScheme: darkDynamic,
-            useMaterial3: true,
-          ),
-          home: const LoadingScreen(),
-        );
-      },
+    return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: const LoadingScreen(),
     );
   }
 }
